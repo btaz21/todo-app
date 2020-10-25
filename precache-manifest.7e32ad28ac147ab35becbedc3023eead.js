5d951f2d49fb1cb107dd34903e7f41130a86f050
Bobby Tazioli <bobby@Bobbys-MacBook-Pro.local>
2020-10-25 14:45:13 -0500

    Updates

---
+++ precache-manifest.7e32ad28ac147ab35becbedc3023eead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3e3cc46906c7ba0732a14cae1a4643",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "5a3307681800f568ffcc",
+    "url": "/todo-app/static/css/main.6c9b3c92.chunk.css"
+  },
+  {
+    "revision": "08e7066eb0d8ac13cc06",
+    "url": "/todo-app/static/js/2.f4ae1bf4.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/todo-app/static/js/2.f4ae1bf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3307681800f568ffcc",
+    "url": "/todo-app/static/js/main.57817331.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);
